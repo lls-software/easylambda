30fa7354f410a25a98a85a294d6bf9a286d0be1b
Leandro Lima <leandro@lls-software.com>
2024-10-20 15:44:51 -0300

    add body parameters test

--- examples/test_param_body.py
+++ examples/test_param_body.py
@@ -1,3 +1,5 @@
+import json
+from secrets import token_hex
 from typing import Annotated
 
 from easylambda import post
@@ -15,3 +17,138 @@ class Item(BaseModel):
 @post("/items")
 def lambda_handler(item: Annotated[Item, Body]) -> dict:
     return item.model_dump()
+
+
+def test_success() -> None:
+    response = lambda_handler(
+        {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/items",
+            "rawQueryString": "",
+            "cookies": [],
+            "headers": {
+                "content-type": "application/json",
+            },
+            "queryStringParameters": {},
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": None,
+                "domainName": "url-id.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "url-id",
+                "http": {
+                    "method": "POST",
+                    "path": "/items",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent",
+                },
+                "requestId": token_hex(),
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390,
+            },
+            "body": '{"name": "item", "price": 9.99}',
+            "pathParameters": None,
+            "isBase64Encoded": False,
+            "stageVariables": None,
+        },
+        object(),
+    )
+
+    assert response["statusCode"] == 200
+    assert json.loads(response["body"]) == {
+        "name": "item",
+        "description": None,
+        "price": 9.99,
+        "tax": None,
+    }
+
+
+def test_unparseable_json() -> None:
+    response = lambda_handler(
+        {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/items",
+            "rawQueryString": "",
+            "cookies": [],
+            "headers": {
+                "content-type": "application/json",
+            },
+            "queryStringParameters": {},
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": None,
+                "domainName": "url-id.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "url-id",
+                "http": {
+                    "method": "POST",
+                    "path": "/items",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent",
+                },
+                "requestId": token_hex(),
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390,
+            },
+            "body": "invalid",
+            "pathParameters": None,
+            "isBase64Encoded": False,
+            "stageVariables": None,
+        },
+        object(),
+    )
+
+    assert response["statusCode"] == 422
+
+
+def test_invalid_attributes() -> None:
+    response = lambda_handler(
+        {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/items",
+            "rawQueryString": "",
+            "cookies": [],
+            "headers": {
+                "content-type": "application/json",
+            },
+            "queryStringParameters": {},
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": None,
+                "domainName": "url-id.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "url-id",
+                "http": {
+                    "method": "POST",
+                    "path": "/items",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent",
+                },
+                "requestId": token_hex(),
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390,
+            },
+            "body": "{}",
+            "pathParameters": None,
+            "isBase64Encoded": False,
+            "stageVariables": None,
+        },
+        object(),
+    )
+
+    assert response["statusCode"] == 422
