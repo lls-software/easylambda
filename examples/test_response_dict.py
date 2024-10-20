d263d54f9901e4424b479adf6c99ffcc2607d08a
Leandro Lima <leandro@lls-software.com>
2024-10-20 15:52:50 -0300

    add test for dict response

--- examples/test_response_dict.py
+++ examples/test_response_dict.py
@@ -1,6 +1,51 @@
+import json
+
 from easylambda import get
 
 
 @get("/")
 def lambda_handler() -> dict:
     return {"message": "Hello World!"}
+
+
+def test_success() -> None:
+    response = lambda_handler(
+        {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/",
+            "rawQueryString": "",
+            "cookies": [],
+            "headers": {},
+            "queryStringParameters": {},
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": None,
+                "domainName": "url-id.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "url-id",
+                "http": {
+                    "method": "GET",
+                    "path": "/",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent",
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390,
+            },
+            "body": "",
+            "pathParameters": None,
+            "isBase64Encoded": False,
+            "stageVariables": None,
+        },
+        object(),
+    )
+
+    assert response["statusCode"] == 200
+    assert response["headers"]["Content-Type"] == "application/json"
+    assert json.loads(response["body"]) == {"message": "Hello World!"}
