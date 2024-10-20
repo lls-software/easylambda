e31ed5eb7cd34cef2abac4bb6cc0b078abc70c55
Leandro Lima <leandro@lls-software.com>
2024-10-20 15:52:07 -0300

    add test for custom response

--- examples/test_response_custom.py
+++ examples/test_response_custom.py
@@ -8,3 +8,45 @@ def lambda_handler() -> Response:
         statusCode=418,
         body="I'm a teapot",
     )
+
+
+def test_success() -> None:
+    response = lambda_handler(
+        {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/",
+            "rawQueryString": "",
+            "cookies": [],
+            "headers": {},
+            "queryStringParameters": {},
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": None,
+                "domainName": "url-id.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "url-id",
+                "http": {
+                    "method": "GET",
+                    "path": "/",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent",
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390,
+            },
+            "body": "",
+            "pathParameters": None,
+            "isBase64Encoded": False,
+            "stageVariables": None,
+        },
+        object(),
+    )
+
+    assert response["statusCode"] == 418
+    assert response["body"] == "I'm a teapot"
