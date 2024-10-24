9665307baa98e9fe21a4538969e5819a42a95a4a
Leandro Lima <leandro@lls-software.com>
2024-10-24 16:53:33 -0300

    add example and test for 204 response

---
+++ examples/test_response_none.py
@@ -0,0 +1,47 @@
+from easylambda import get
+
+
+@get("/")
+def lambda_handler() -> None: ...
+
+
+def test_success() -> None:
+    response = lambda_handler(
+        {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/",
+            "rawQueryString": "",
+            "cookies": [],
+            "headers": {},
+            "queryStringParameters": {},
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": None,
+                "domainName": "url-id.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "url-id",
+                "http": {
+                    "method": "GET",
+                    "path": "/",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent",
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390,
+            },
+            "body": "",
+            "pathParameters": None,
+            "isBase64Encoded": False,
+            "stageVariables": None,
+        },
+        object(),
+    )
+
+    assert response["statusCode"] == 204
+    assert response["body"] == ""
